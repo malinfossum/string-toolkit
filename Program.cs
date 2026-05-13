bccd68493ee0819dc2c14825c848f86ba0066284
CozREV <kristianafg@proton.me>
2026-05-13 10:59:34 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
-﻿namespace StringToolkit;
+namespace StringToolkit;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        String input;
+
+
+        Console.Write("Enter a string: ");
+        input = Console.ReadLine();
+
+        Console.WriteLine("Reverse (1) - Uppercase (2) - Lowercase (3)");
+        input = Console.ReadLine();
+
+
+
     }
 }
