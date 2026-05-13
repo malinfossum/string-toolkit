b2d9d41c3de3c2ed05b0318d7e26df928af7396e
CozREV <kristianafg@proton.me>
2026-05-13 12:05:54 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -4,19 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Console.Write("Enter a string: ");
+        //string input = Console.ReadLine();
 
-        Console.Write("Enter a string: ");
-        string input = Console.ReadLine();
+        //Console.WriteLine("Reverse (1) - Uppercase (2) - Lowercase (3)");
+        //input = Console.ReadLine();
 
-        Console.WriteLine("Reverse (1) - Uppercase (2) - Lowercase (3)");
-        input = Console.ReadLine();
 
-        string reversed = "";
-        for (int i = input.Length - 1; i >= 0; i--)
-        {
-            reversed += input[i];
-        }
+        //switch (input)
+        //{
+        //    case 1:
+        //        string reversed = "";
+        //                for (int i = input.Length - 1; i >= 0; i--)
+        //                {
+        //                    reversed += input[i];
+        //                }
+        //        break;
 
-        Console.WriteLine(reversed);
+        //    case 2:
+        //        input = input.toUpper();
+        //        break;
+
+        //    case 3:
+        //        input = input.toLower()
+        //        break;
+        //}
+
+        
+        //Console.WriteLine(input);
     }
 }
